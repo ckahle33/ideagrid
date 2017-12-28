e1f0865ab1341dc7a6bed9ac2333b9159131b6f5
cpk <ckahle33@gmail.com>
2017-12-28 03:41:07 -0600

    login polish

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,11 @@
 class SessionsController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
-    @user = User.where(params[:email]).first
-    if @user && @user.authenticate(params[:password])
+    @user = User.where(email: session_params[:email]).first
+    if @user && @user.authenticate(session_params[:password])
       session[:user_id] = @user.id
       redirect_to root_path
       flash[:info] = "Logged In."
@@ -17,5 +18,9 @@ class SessionsController < ApplicationController
   def destroy
     session[:user_id] = nil
     redirect_to login_path
+  end
+
+  def session_params
+    params.require(:user).permit(:password, :email)
   end
 end
